f0a5fedd0326e4e4464e442f14ffed089f368763
pratyush kumar <pratyush845@gmail.com>
2018-02-21 15:42:11 +0530

    Added date format

--- src/DateConversion.java
+++ src/DateConversion.java
@@ -4,8 +4,7 @@ import java.text.SimpleDateFormat;
 public class DateConversion {
     public static void main(String[] args) {
         SimpleDateFormat sdf=   new SimpleDateFormat("dd/MM/yyyy");
-String date=sdf.format(new Date());
+        String date=sdf.format(new Date());
         System.out.println(date);
     }
-
 }
